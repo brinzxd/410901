7656e4d72ffe2b832987bd3abbb5a9225b79793c
brinzxd <zdanov1chpl@mail.ru>
2024-09-12 21:11:14 +0300

    init commit

---
+++ lab03/lab03.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 100; i <= 999; i++) {
+        if (i / 100 == i % 10) {
+            cout << i << " ";
+        }
+        else continue;
+    }
+}
---
+++ lab04/lab04.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, c, f=0;
+    cin >> n >> c;
+    int *array = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+    for (int j = 0; j < n; j++) {
+        if (array[j] < c) {
+            f++;
+        }
+    }
+    cout << f;
+}
